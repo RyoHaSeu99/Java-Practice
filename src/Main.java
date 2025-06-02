791d8101fdb01e5be22f9212fc8f1254077eaaf8
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-02 11:24:25 +0900

    증감 연산자

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 10, b = 3;
+       int a = 10, b = 3;
 
-        a += 5; // a = a + 5(계산기 등에 이용 가능할듯
+       ++a;     // 전위형
         System.out.println(a);
-        // a = 15, b = 3
-        b -= 2;
+
+        --b;
         System.out.println(b);
-        // a = 15, b = 1
-        a *= b;
+
+        a++;    // 후위형
         System.out.println(a);
-        // a = 15, b = 1
-        a /= 7;
-        System.out.println(a);
-        // a = 2, b = 1
-        a %= 8;
-        System.out.println(a);
-        // a = 2, b = 1
+
+        b--;
+        System.out.println(b);
+
+        System.out.println(++a);
+        System.out.println(a++);    // 사용하고있지 않은 변수는 회색으로 표시됨
+        // 후위형은 로직이 처리된 후 값이 증가하기 때문에
+        // 증감연산자가 적용되지 않음
+        // System.out.println(a);
     }
 }
