8ab635a3c314e30ef5e43555890b6b82b115303c
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-02 10:04:03 +0900

    상수

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        //변수 - 변할 수 있음
-        int a = 10;
 
-        a = 11;
+        final int DAYS_IN_WEEK = 7; // 상수: 값이 변할 수 없음
 
-        System.out.println(a);
+//        DAYS_IN_WEEK = 11; 에러 발생
 
+        System.out.println(DAYS_IN_WEEK);
+//        final int DAYS_IN_WEEK = 7;
+//        final double PI = 3.14159;
+//        final String GREETING = "Hello, world!";
+//
+//        System.out.println("Days in a week: " + DAYS_IN_WEEK);
+//        System.out.println("Value of PI: " + PI);
+//        System.out.println("Greeting: " + GREETING);
+//
+//        // Error:
+//        // DAYS_IN_WEEK = 8;
 
     }
 }
