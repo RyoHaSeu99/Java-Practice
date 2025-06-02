6b082891e5c043d4d2e047cc69bf74459d0a2e79
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-02 13:58:56 +0900

    조건문

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-       int a = 10, b = 3;
 
-       ++a;     // 전위형
-        System.out.println(a);
+      int number = -1;
 
-        --b;
-        System.out.println(b);
-
-        a++;    // 후위형
-        System.out.println(a);
-
-        b--;
-        System.out.println(b);
-
-        System.out.println(++a);
-        System.out.println(a++);    // 사용하고있지 않은 변수는 회색으로 표시됨
-        // 후위형은 로직이 처리된 후 값이 증가하기 때문에
-        // 증감연산자가 적용되지 않음
-        // System.out.println(a);
+      if (number > 0) {
+          System.out.println("number는 0보다 큽니다.");
+      } else if (number == 0) {
+          System.out.println("number는 0과 같습니다.");
+      } else {
+          System.out.println("number는 0보다 작습니다");
+      }
     }
 }
