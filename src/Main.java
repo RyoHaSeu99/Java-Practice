7a11460edf4cfea7aa97cad5883eae7767650111
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-04 09:14:00 +0900

    변수

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        // "happy", "angry", "sad", etc.
-        String mood = "good";
+        byte b = 10;
+        System.out.println(b);
 
-        switch (mood) {
-            case "happy":   // if (mood == "happy") {System.out.println("나는 행복하다. 왜냐하면 행복하기 때문이다."); }
-                System.out.println("나는 행복하다. 왜냐하면 행복하기 때문이다.");
-                break;
-            case "angry":
-                System.out.println("나는 화가난다. 왜냐하면 화가나기 때문이다.");
-                break;
-            case "sad":
-                System.out.println("나는 슬프다. 왜냐하면 슬프기 때문이다.");
-                break;
-            default:
-                System.out.println("지금 내 기분은 이 단어들로는 표현할 수 없다.");
+        short s = 1000;
+        System.out.println(s);
 
+        int age = 25;
+        System.out.println(age);
 
-//        String day = "SUNDAY" ;
-//
-//        switch (day) {
-//            case "MONDAY":
-//            case "TUESDAY":
-//            case "WEDNESDAY":
-//            case "THURSDAY":
-//            case "FRIDAY":
-//                System.out.println("Weekday");
-//                break;
-//            case "SATURDAY":
-//            case "SUNDAY":
-//                System.out.println("Weekend");
-//                break;
+        long count = 10000000000L;
+        System.out.println(count);
 
-        }
+        float pi = 3.14F;
+        System.out.println(pi);
 
+        double salary = 5000.5;
+        System.out.println(salary);
 
+        char grade = 'A';
+        System.out.println(grade);
+
+        boolean isValid = true;
+        System.out.println(isValid);
     }
 }
