8abc78e83496370f1f117b53409ef3eb5f963d06
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-04 14:21:02 +0900

    반복문(for)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-       int a = 10, b = 7;
 
-        System.out.println("a == b : " + (a == b));
-        System.out.println("a != b : " + (a != b));
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println(i);
+//        }
+//
+//        for (int i = 10; i > 0; i--) {
+//            System.out.println(i);
+//        }
 
-        System.out.println("a > b : " + (a > b));
-        System.out.println("a < b : " + (a < b));
-        System.out.println("a >= 10 : " + (a >= 10));
-        System.out.println("b <= 5 : " + (b <= 5));
+
+//        int number = 9;
+//        for (int i = 1; i <= 9 ; i++) {
+//            System.out.println(number + " x " + i + " = " + (number * i));
+//        }
 
 
+//        int number = 5;
+//
+//        for (int i = 1; i <= 19; i++) {
+//            if (i >= 5) {
+//                System.out.println(number + " x " + i + " = " + (number * i));
+//            }
+//        }
+
+//        for (int i = 1; i <= 20; i++) {
+//            if (i % 3 == 0) {
+//                System.out.println("i 는 " + i);
+//            }
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("구구단 " + i + "단");
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println();
+        }
+
+//        for (int i = 1; i <= 9; i++) {
+//            for (int j = 1; j <= 9; j++) {
+//                System.out.println("i는 " + i + ", j는 " + j);
+//            }
+//
+//        }
     }
 }
