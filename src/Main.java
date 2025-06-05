06e9fac95735994cace54ffade80d74cf1018788
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-05 14:27:22 +0900

    함수

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,16 @@
 import java.sql.SQLOutput;
 
 public class Main {
-    public static void main(String[] args) {
-//        int a = 10;
-//        int b = 13;
-//
-//
-//        int max = (a > b) ? a : b;
-//
-//        int max;
-//        if (a > b) {
-//            max = a;
-//        } else {
-//            max = b;
-//        }
-//
-//        System.out.println(max);
-//        int score = 64;
-//
-//        String result = (score > 60) ? "통과" : "탈락";
-//        System.out.println("결과: " + result);
-
-//        int score = 64;
-//        String result;
-//        if (score > 60) {
-//            result = "통과";
-//        } else {
-//            result = "탈락";
-//        }
-//        System.out.println("결과: " + result);
-
-        String color = "보라";
-
-//        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : "보라";
-//
-//        System.out.println(choiceColor);
-
-//        if (color == "빨강") {
-//            System.out.println("빨강");
-//        } else if (color == "파랑") {
-//            System.out.println("파랑");
-//        } else {
-//            System.out.println("보라");
-//        }
-
-
+    public static void printStudent(String name, int age) {
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age);
+        System.out.println("--------------");
     }
+    public static void main(String[] args) {
+        System.out.println("=== 학생 정보 출력 ===");
+        Main.printStudent("Alice", 20);
+        printStudent("Bob", 22);
+        printStudent("Charlie", 25);
+    }
+
 }
