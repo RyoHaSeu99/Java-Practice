de81a1707e812563964caabb93fb57112529e783
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-05 16:05:15 +0900

    유니코드, 아스키코드

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 import java.sql.SQLOutput;
 
 public class Main {
-    public static int addNumber (int a, int b) {
-        int result = a + b;
 
-        return result;
-    }
     public static void main(String[] args) {
-        int number = addNumber(10, 7);
-        int number2 = addNumber(14, 1);
-        int number3 = addNumber(152, 3241);
-        System.out.println(number);
-        System.out.println(number2);
-        System.out.println(number3);
+        char asciChar = 'A';
+        System.out.println("Character: " + asciChar);
+
+        int asciCode = (int) asciChar;
+        System.out.println("ASCII Code: " + asciCode);
+
+        char koreanChar = '가';
+        System.out.println("Character: " + koreanChar);
+
+        int unicodeCode = (int) koreanChar;
+        System.out.println("Unicode: U+" + Integer.toHexString(unicodeCode).toUpperCase());
     }
 }
+// 48 50 49 49 50 50
