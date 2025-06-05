0504d4b1a7cec8160c1e33c557b1455ee97e352f
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-05 11:50:55 +0900

    삼항 연산자

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,47 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-//        for (int i = 1; i <= 10; i++) {
-//            if (i == 7) {
-//                break;
-//            }
-//            System.out.println(i);
+//        int a = 10;
+//        int b = 13;
+//
+//
+//        int max = (a > b) ? a : b;
+//
+//        int max;
+//        if (a > b) {
+//            max = a;
+//        } else {
+//            max = b;
 //        }
-//        for (int i = 1; i <= 10; i++) {
-//            if (i %3 != 0) {
-//                continue;
-//            }
-//            System.out.println(i);
-//        }
+//
+//        System.out.println(max);
+//        int score = 64;
+//
+//        String result = (score > 60) ? "통과" : "탈락";
+//        System.out.println("결과: " + result);
 
-        for (int i = 1; i <= 20; i++) {
-            if (i >= 15) {
-                break;
-            } else if (i % 5 == 0) {
-                continue;
-            }
-            System.out.println(i);
-        }
+//        int score = 64;
+//        String result;
+//        if (score > 60) {
+//            result = "통과";
+//        } else {
+//            result = "탈락";
+//        }
+//        System.out.println("결과: " + result);
+
+        String color = "보라";
+
+//        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : "보라";
+//
+//        System.out.println(choiceColor);
+
+//        if (color == "빨강") {
+//            System.out.println("빨강");
+//        } else if (color == "파랑") {
+//            System.out.println("파랑");
+//        } else {
+//            System.out.println("보라");
+//        }
 
 
     }
